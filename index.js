15b05c729323a851cf37368e99266f8004bbd557
Enrico Marino <enrico.marino@email.com>
2013-07-03 18:51:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
-/**
- * next
- * Get the next sibling
- * 
- * @return {Element} next sibling
- * @api public
- */
+module.exports = function (element) { 
+  
+  /**
+   * next
+   * Get the next sibling
+   * 
+   * @return {Element} next sibling
+   * @api public
+   */
+  
+  element.prototype.next = function () {
+    return this.el.nextSibling;
+  };
+  
+  /**
+   * previous
+   * Get the previous sibling
+   * 
+   * @return {Element} previous sibling
+   * @api public
+   */
+  
+  element.prototype.previous = function () {
+    return this.el.previousSibling;
+  };
+  
+  /**
+   * parent
+   * Get the parent node
+   * 
+   * @return {Element} parent
+   * @api public
+   */
+  
+  element.prototype.parent = function () {
+    return this.el.parentNode;
+  };
 
-exports.next = function () {
-  return this.el.nextSibling;
-};
-
-/**
- * previous
- * Get the previous sibling
- * 
- * @return {Element} previous sibling
- * @api public
- */
-
-exports.previous = function () {
-  return this.el.previousSibling;
-};
-
-/**
- * parent
- * Get the parent node
- * 
- * @return {Element} parent
- * @api public
- */
-
-exports.parent = function () {
-  return this.el.parentNode;
+  return element;
 };
